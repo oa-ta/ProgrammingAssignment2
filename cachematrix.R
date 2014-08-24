a61e1559ef7bf724e021a7cdc7db05b7ddec3055
oa-ta <turzanska.olga@yahoo.com>
2014-08-24 21:35:48 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## Initializing a variable
+    s <- NULL
+    
+    ## Set method to set matrix
+    set <- function(matrix) {
+            x <<- matrix
+            s <<- NULL
+    }
+    
+    ## Get method to get matrix
+    get <- function() x
+    
+    ## Setinverse to set inverse matrix
+    setinverse <- function(inverse) s <<- inverse
+    
+    ## Getinverse to set inverse matrix
+    getinverse <- function() s
+    
+    ## List to return
+    list(set = set, 
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
